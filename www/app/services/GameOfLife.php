3d394c0c64f657c2eb27ba172f37914c21f0f390
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-12 15:31:24 +0400

    count neighbours function implemented

--- www/app/services/GameOfLife.php
+++ www/app/services/GameOfLife.php
@@ -16,8 +16,24 @@ class GameOfLife implements GameOfLifeInterface {
         return [];
     }
 
-    public function countNeighbours($population = [], $i = 0, $j = 0) : int {
-        return 0;
+    public function countNeighbours($population = [], $x = 0, $y = 0) : int {
+
+        $count = 0;
+
+        for ($i = -1; $i <= 1;$i++) {
+            if (isset($population[$y + $i])) {
+                for ($j = -1; $j <= 1;$j++) {
+                    if ($i != 0 || $j != 0) { // if it's not the central cell we're looking at
+                        if (isset($population[$y + $i][$x + $j]) && $population[$y + $i][$x + $j]) {
+                            $count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $count;
+
     }
 
     public function getCellNextGeneration($cell = false, $neighbours = 0) : bool {
