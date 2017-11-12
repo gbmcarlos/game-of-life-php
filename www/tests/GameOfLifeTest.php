ed67901821650986aa34cd46d23b05f82d01cee3
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-12 13:07:29 +0400

    test for counting neighbours implemented

--- www/tests/GameOfLifeTest.php
+++ www/tests/GameOfLifeTest.php
@@ -40,6 +40,64 @@ class GameOfLifeTest extends TestCase {
 
     }
 
+    public function testCountNeighbours() {
+
+        $this->assertEquals(0, $this->game->countNeighbours([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(1, $this->game->countNeighbours([
+            [1, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(2, $this->game->countNeighbours([
+            [1, 1, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(3, $this->game->countNeighbours([
+            [1, 1, 1],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(4, $this->game->countNeighbours([
+            [1, 1, 1],
+            [1, 0, 0],
+            [0, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(5, $this->game->countNeighbours([
+            [1, 1, 1],
+            [1, 0, 1],
+            [0, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(6, $this->game->countNeighbours([
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 0, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(7, $this->game->countNeighbours([
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 0]
+        ]), 1, 1);
+
+        $this->assertEquals(8, $this->game->countNeighbours([
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]
+        ]), 1, 1);
+
+    }
+
     public function tearDown() {
         unset($this->game);
     }
