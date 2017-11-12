42c804fea53b76b2338108cff43517652dfe4125
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-12 12:59:43 +0400

    game of life class created with basics empty methods

---
+++ www/app/services/GameOfLife.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gbmcarlos
+ * Date: 11/12/17
+ * Time: 12:36 PM
+ */
+
+namespace App\services;
+
+use App\services\GameOfLifeInterface;
+
+class GameOfLife implements GameOfLifeInterface {
+
+    public function getNextGeneration($population = []) : array {
+
+    }
+
+    public function countNeigbours($population = [], $i = 0, $j = 0) : int {
+
+    }
+
+    public function getCellNextGeneration($cell = false, $neighbours = 0) : bool {
+
+    }
+
+}
