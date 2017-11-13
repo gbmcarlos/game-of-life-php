103cf2ce3262917c527e390da52e46ee6d1ba303
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-13 12:02:30 +0400

    assertion for the new constraint added to the population test case

--- www/tests/PopulationAssertion/PopulationTestCase.php
+++ www/tests/PopulationAssertion/PopulationTestCase.php
@@ -12,12 +12,20 @@ use PHPUnit\Framework\TestCase;
  */
 abstract class PopulationTestCase extends TestCase {
 
+    public static function assertPopulationDimensions(int $expectedX, int $expectedY, $actual) {
+        self::assertThat($actual, self::populationDimensions($expectedX, $expectedY));
+    }
+
+    public static function populationDimensions(int $expectedX, int $expectedY) {
+        return new Constraint_PopulationDimensions($expectedX, $expectedY);
+    }
+
     public static function assertPopulationEquals($expected, $actual) {
         self::assertThat($actual, self::populationEquals($expected));
     }
 
     public static function populationEquals($expected) {
-    return new Constraint_PopulationEquals($expected);
-}
+        return new Constraint_PopulationEquals($expected);
+    }
 
 }
