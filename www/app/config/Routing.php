858144123e977a22d96b263f0a6dffdbf83d7215
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-13 18:51:56 +0400

    routing for the random pattern and gosper's glider gun routes

--- www/app/config/Routing.php
+++ www/app/config/Routing.php
@@ -15,8 +15,14 @@ class Routing {
     public static function registerRoutes(Application $app){
 
         // Register routes here
-        $app->get('/', "FrontController:helloWorld")
-            ->bind('hellorWorld');
+        $app->get('/', "FrontController:index")
+            ->bind('index');
+
+        $app->get('/random-pattern', "FrontController:randomPattern")
+            ->bind('random-pattern');
+
+        $app->get('/gospers-glider-gun', "FrontController:gospersGliderGun")
+            ->bind('gospers-glider-gun');
 
     }
 
