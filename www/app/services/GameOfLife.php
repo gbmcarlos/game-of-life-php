e2980b6a6ab92c6bc79b72f9dbb30d149556c1d7
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-13 10:25:43 +0400

    countNeighbours method fixed, and getNextGeneration improved

--- www/app/services/GameOfLife.php
+++ www/app/services/GameOfLife.php
@@ -13,10 +13,37 @@ use App\services\GameOfLifeInterface;
 class GameOfLife implements GameOfLifeInterface {
 
     public function getNextGeneration($population = []) : array {
-        return [];
+
+        $result = [];
+
+        for ($i = 0; $i < count($population); $i++) {
+
+            if (is_array($population[$i])) {
+
+                $result[$i] = [];
+                for ($j = 0; $j < count($population[$i]); $j++) {
+
+                    $neighbours = $this->countNeighbours($population, $i, $j);
+
+                    $nextCell = $this->getCellNextGeneration($population[$i][$j], $neighbours);
+
+                    $result[$i][$j] = $nextCell;
+
+                }
+            } else {
+
+                $neighbours = $this->countNeighbours($population, 0, $i);
+                $nextCell = $this->getCellNextGeneration($population[$i][0], $neighbours);
+
+                $result[$i] = $nextCell;
+            }
+        }
+
+        return $result;
+
     }
 
-    public function countNeighbours($population = [], $x = 0, $y = 0) : int {
+    public function countNeighbours($population = [], $y = 0, $x = 0) : int {
 
         $count = 0;
 
@@ -24,7 +51,7 @@ class GameOfLife implements GameOfLifeInterface {
             if (isset($population[$y + $i])) {
                 for ($j = -1; $j <= 1;$j++) {
                     if ($i != 0 || $j != 0) { // if it's not the central cell we're looking at
-                        if (isset($population[$y + $i][$x + $j]) && $population[$y + $i][$x + $j]) {
+                        if (isset($population[$y + $i][$x + $j]) && $population[$y + $i][$x + $j]) { //x is the horizontal, y is the vertical
                             $count++;
                         }
                     }
