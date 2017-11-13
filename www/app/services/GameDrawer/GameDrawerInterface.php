77695ebb340fd351524c7e7c9d79319e15e353b4
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-11-13 18:49:00 +0400

    game drawer interface expanded

--- www/app/services/GameDrawer/GameDrawerInterface.php
+++ www/app/services/GameDrawer/GameDrawerInterface.php
@@ -6,13 +6,16 @@
  * Time: 12:21 PM
  */
 
-namespace App\services;
-
+namespace App\services\GameDrawer;
 
 interface GameDrawerInterface {
 
-    public function drawPopulation($grid = []) : \SplTempFileObject;
+    public function drawPopulation($grid = []);
 
-    public function drawGame($gridIterations = []) : \SplTempFileObject;
+    public function drawGame(array $grid, int $iterations) : string;
+
+    public function getImageBinaryData($image) : string;
+
+    public function saveTemporary($imageBin);
 
 }
